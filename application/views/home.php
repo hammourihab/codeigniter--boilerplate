7f847cb4dccb973be5a4f83920f28c2434cc054c
Hammour ihab <hammourihab@yandex.com>
2019-02-17 17:08:11 +0100

    updated home view

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Codeigniter3 | Boilerplate</title>
+		<link rel="icon" href="/assets/images/favicon.png">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<!-- Stylesheets here --> 
+	</head>
+	<body>
+
+		<!-- Scripts here -->
+	</body>
+</html> 
